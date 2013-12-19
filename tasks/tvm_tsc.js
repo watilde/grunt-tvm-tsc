81bb2a752560edd9a02f99673dea4d41fa66c17f
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-12-19 10:32:32 +0900

    Update tasks

--- tasks/tvm_tsc.js
+++ tasks/tvm_tsc.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
   'use strict';
+  var taskName = 'tvm-tsc';
+  var description = 'Compile TypeScript files into JavaScript with tvm';
+  var taskFunction = function () {};
 
-  grunt.registerMultiTask('tvm-tsc', 'Compile TypeScript files into JavaScript with tvm', function() {
-  });
+
+  grunt.registerMultiTask(taskName, description, taskFunction);
 };
