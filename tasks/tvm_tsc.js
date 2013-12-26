cf863c7a32c8545f7c1529226f5db7eda6ae649b
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-12-26 21:41:47 +0900

    Add tvm

--- tasks/tvm_tsc.js
+++ tasks/tvm_tsc.js
@@ -1,8 +1,11 @@
 module.exports = function(grunt) {
   'use strict';
-  var taskName = 'tvm-tsc';
+  var taskName = 'tvm_tsc';
   var description = 'Compile TypeScript files into JavaScript with tvm';
-  var taskFunction = function () {};
+  var async = require('async');
+  var tvm = require('tvm');
+  var taskFunction = function () {
+  };
 
 
   grunt.registerMultiTask(taskName, description, taskFunction);
