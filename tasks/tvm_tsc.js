7c155c7489151fd85ae0b68d731c879db91b6966
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-01-10 19:21:20 +0900

    Fixes asynchronous tvm_tsc

--- tasks/tvm_tsc.js
+++ tasks/tvm_tsc.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
       });
     };
 
-    var tvm_tsc = function (version, validFiles, destFiles) {
+    var tvm_tsc = function (version, validFiles, destFiles, end) {
       async.waterfall([function (callback) {
         var exists = fs.existsSync(tvm.dirname.typescript + 'v' + version);
         if (exists) {
@@ -32,20 +32,22 @@ module.exports = function(grunt) {
         }
       }, function (callback) {
         tvm.use(options.version, callback);
+      }, function (callback) {
+        tvm.tsc(validFiles + ' --out ' + destFiles, callback);
       }, function () {
-        tvm.tsc(validFiles + ' --out ' + destFiles);
         grunt.log.writeln('File ' + destFiles + ' created.');
+        end();
       }]);
     };
 
-    var done = this.async();
-
-    this.files.forEach(function (f) {
-      var validFiles = removeInvalidFiles(f);
-      var destFiles  = f.dest;
+    var done  = this.async();
+    var files = this.files;
+    async.forEachSeries(files, function (file, callback) {
+      var validFiles = removeInvalidFiles(file);
+      var destFiles  = file.dest;
       var version    = options.version;
-      tvm_tsc(version, validFiles, destFiles);
-    });
+      tvm_tsc(version, validFiles, destFiles, callback);
+    }, done);
   };
 
   grunt.registerMultiTask(taskName, description, taskFunction);
